b114922817a7bf3dffc8dd33f56089bd40f96ad7
phongpakornthun-debug <phongpakorn.thun@bumail.net>
2026-03-25 22:07:47 +0700

    Add Win UI

---
+++ Assets/Scrip/Win.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneOnHit : MonoBehaviour
+{
+    [Header("Scene Settings")]
+    [SerializeField] private string sceneName;
+
+    [Header("Trigger Settings")]
+    [SerializeField] private string targetTag = "Player"; 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(targetTag))
+        {
+            LoadScene();
+        }
+    }
+
+    void LoadScene()
+    {
+        if (!string.IsNullOrEmpty(sceneName))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+
+    }
+}
